462c471a220e2dd459ea7e329015a0375981ddf1
thiago <thiago.thcs@hotmail.com>
2024-02-27 14:50:55 -0300

    modifiquei comentarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,9 +20,3 @@ import { HomeComponent } from './home/home.component';
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
